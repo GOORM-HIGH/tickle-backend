e2e21b57b4f32a433b440bfab4273a4c362a7fec
서해윤 <haenni@seohaeyun-ui-MacBookAir.local>
2025-07-29 17:24:20 +0900

    feat: 채팅 도메인 엔티티 설계

---
+++ src/main/java/com/profect/tickle/domain/chat/entity/Chat.java
@@ -0,0 +1,62 @@
+package com.profect.tickle.domain.chat.entity;
+
+import com.profect.tickle.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "chat")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "chat_room_id", nullable = false)
+    private Long chatRoomId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "chat_message_type", nullable = false)
+    private ChatMessageType messageType;
+
+    @Column(name = "chat_content", length = 255, nullable = false)
+    private String content;
+
+    @Column(name = "chat_file_path", length = 255)
+    private String filePath;
+
+    @Column(name = "chat_file_name", length = 255)
+    private String fileName;
+
+    @Column(name = "chat_file_size")
+    private Integer fileSize;
+
+    @Column(name = "chat_file_type", length = 100)
+    private String fileType;
+
+    @Column(name = "chat_is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @Column(name = "chat_is_edited", nullable = false)
+    private Boolean isEdited;
+
+    @Column(name = "chat_edited_at")
+    private LocalDateTime editedAt;
+
+    @Column(name = "chat_created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "chat_sender_status", nullable = false)
+    private Boolean senderStatus; // true: 받음, false: 안 받음
+}
---
+++ src/main/java/com/profect/tickle/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,40 @@
+package com.profect.tickle.domain.chat.entity;
+
+import com.profect.tickle.domain.performance.entity.Performance;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "chat_room")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "performance_id", nullable = false)
+    private Performance performance;
+
+    @Column(name = "chat_room_name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "chat_room_status", nullable = false)
+    private Boolean isActive;  // true: 열림, false: 종료
+
+    @Column(name = "chat_room_max_participants", nullable = false)
+    private Short maxParticipants; // default 100, 최대 200명 예상
+
+    @Column(name = "chat_room_created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "chat_room_updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
