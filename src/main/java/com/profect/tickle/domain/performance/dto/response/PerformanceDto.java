182100daf73b59b4bb1ef872738ace11d25c02e9
ju2hong <juihong730@gmail.com>
2025-07-31 17:35:09 +0900

    feat: 공연 응답 dto 생성

---
+++ src/main/java/com/profect/tickle/domain/performance/dto/response/PerformanceDto.java
@@ -0,0 +1,19 @@
+package com.profect.tickle.domain.performance.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PerformanceDto {
+    private Long performanceId;
+    private String title;
+    private LocalDateTime date;
+    private String img;
+}
