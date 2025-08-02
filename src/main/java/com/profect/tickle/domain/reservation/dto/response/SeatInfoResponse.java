4b9891a33fb47bfff683f54dd5f95df7dd33f570
shimbaa <shim9597@gmail.com>
2025-08-02 23:15:32 +0900

    feat: 좌석 정보 응답 dto 구현

---
+++ src/main/java/com/profect/tickle/domain/reservation/dto/response/SeatInfoResponse.java
@@ -0,0 +1,17 @@
+package com.profect.tickle.domain.reservation.dto.response;
+
+import com.profect.tickle.domain.reservation.entity.SeatGrade;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SeatInfoResponse {
+    private Long seatId;
+    private String seatNumber;        // 좌석 번호 (A1, B2 등)
+    private SeatGrade seatGrade;      // 좌석 등급 (VIP, R, S)
+    private Integer seatPrice;        // 좌석 가격
+    private Long statusId;            // DB status ID (11: 예매가능, 12: 선점중, 13: 예매완료)
+}
