c23b796258d02b0fd8c1c0c88f52f1ca6f87b358
shimbaa <shim9597@gmail.com>
2025-08-04 16:34:04 +0900

    feat: 회원이 보유한 사용 가능한 쿠폰 조회 쿼리 메서드 추가
    - couponId, memberId, status 조건으로 CouponReceived 조회
    - status 17번(사용 가능 상태)인 경우만 반환

--- src/main/java/com/profect/tickle/domain/member/repository/CouponReceivedRepository.java
+++ src/main/java/com/profect/tickle/domain/member/repository/CouponReceivedRepository.java
@@ -1,10 +1,18 @@
 package com.profect.tickle.domain.member.repository;
 
 import com.profect.tickle.domain.member.entity.CouponReceived;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponReceivedRepository extends JpaRepository<CouponReceived, Long> {
     boolean existsByMemberIdAndCouponId(Long id, Long id1);
+
+    @Query("SELECT cr FROM CouponReceived cr WHERE cr.coupon.id = :couponId " +
+            "AND cr.member.id = :memberId AND cr.status.id = 17")
+    Optional<CouponReceived> findByCouponIdAndMemberIdAndNotUsed(@Param("couponId") Long couponId,
+            @Param("memberId") Long memberId);
 }
