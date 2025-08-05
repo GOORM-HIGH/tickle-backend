18ad6902133fa717580b055c9b5795c2da7ffb1a
shimbaa <shim9597@gmail.com>
2025-08-05 16:15:24 +0900

    at: 예매 내역에 사용될 PerformanceInfo 클래스 추가

---
+++ src/main/java/com/profect/tickle/domain/reservation/dto/response/PerformanceInfo.java
@@ -0,0 +1,18 @@
+package com.profect.tickle.domain.reservation.dto.response;
+
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PerformanceInfo {
+    private Long performanceId;
+    private String title;                       // 공연명
+    private String hall;                       // 공연장
+    private Instant performanceDate;      // 공연 일시
+    private String posterUrl;                   // 포스터 이미지 URL
+    private Short runtime;                   // 공연 시간 (분)
+}
