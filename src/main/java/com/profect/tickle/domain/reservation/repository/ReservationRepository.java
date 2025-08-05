5d5031c6f836a3456f4404396dd0c1d3be6453fd
shimbaa <shim9597@gmail.com>
2025-08-05 14:44:42 +0900

    "feat: 예매 취소 기능을 위한 레포지토리 메서드 추가
    - ReservationRepository: 회원별 예매 조회 메서드 추가
    - SeatRepository: 예매 ID로 좌석 조회 메서드 추가"

--- src/main/java/com/profect/tickle/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/profect/tickle/domain/reservation/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.profect.tickle.domain.reservation.repository;
 
 import com.profect.tickle.domain.reservation.entity.Reservation;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    Optional<Reservation> findByIdAndMemberId(Long reservationId, Long memberId);
 }
