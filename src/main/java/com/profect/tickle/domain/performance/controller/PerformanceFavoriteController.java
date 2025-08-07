4b419f4f091d2ff4adba4923bc5c0bda6baed2b3
ju2hong <juihong730@gmail.com>
2025-08-07 12:56:48 +0900

    feat: 회원별 공원 스크랩 구현

--- src/main/java/com/profect/tickle/domain/performance/controller/PerformanceFavoriteController.java
+++ src/main/java/com/profect/tickle/domain/performance/controller/PerformanceFavoriteController.java
@@ -1,5 +1,6 @@
 package com.profect.tickle.domain.performance.controller;
 
+import com.profect.tickle.domain.performance.dto.response.PerformanceScrapDto;
 import com.profect.tickle.domain.performance.service.PerformanceFavoriteService;
 import com.profect.tickle.global.response.ResultCode;
 import com.profect.tickle.global.response.ResultResponse;
@@ -7,18 +8,21 @@ import com.profect.tickle.global.security.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "공연", description = "공연 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/performance/{performanceId}/scrap")
+@RequestMapping("/api/v1/performance")
 public class PerformanceFavoriteController {
 
     private final PerformanceFavoriteService performanceFavoriteService;
 
-    @PostMapping
+    @PostMapping("/{performanceId}/scrap")
     @PreAuthorize("hasRole('MEMBER')")
     @Operation(summary = "공연 스크랩", description = "해당 공연을 회원이 스크랩합니다.")
     public ResultResponse<Void> scrapPerformance(@PathVariable Long performanceId) {
@@ -27,7 +31,7 @@ public class PerformanceFavoriteController {
         return ResultResponse.ok(ResultCode.PERFORMANCE_SCRAP_SUCCESS);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{performanceId}/scrap")
     @PreAuthorize("hasRole('MEMBER')")
     @Operation(summary = "공연 스크랩 취소", description = "해당 공연에 대한 스크랩을 취소합니다.")
     public ResultResponse<Void> cancelScrap(@PathVariable Long performanceId) {
@@ -35,4 +39,19 @@ public class PerformanceFavoriteController {
         performanceFavoriteService.cancelScrap(performanceId, memberId);
         return ResultResponse.ok(ResultCode.PERFORMANCE_SCRAP_CANCEL_SUCCESS);
     }
+
+    @GetMapping("scrap")
+    public ResponseEntity<List<PerformanceScrapDto>> getMyScraps() {
+        Long memberId = SecurityUtil.getSignInMemberId();
+        List<PerformanceScrapDto> scrapList = performanceFavoriteService.getScrappedPerformances(memberId);
+        return ResponseEntity.ok(scrapList);
+    }
+
+    @GetMapping("/{performanceId}/exists")
+    public ResponseEntity<Boolean> isScrapped(@PathVariable Long performanceId) {
+        Long memberId = SecurityUtil.getSignInMemberId();
+        boolean result = performanceFavoriteService.isScrapped(memberId, performanceId);
+        return ResponseEntity.ok(result);
+    }
+
 }
--- src/main/java/com/profect/tickle/domain/performance/mapper/PerformanceMapper.java
+++ src/main/java/com/profect/tickle/domain/performance/mapper/PerformanceMapper.java
@@ -1,9 +1,6 @@
 package com.profect.tickle.domain.performance.mapper;
 
-import com.profect.tickle.domain.performance.dto.response.GenreDto;
-import com.profect.tickle.domain.performance.dto.response.PerformanceDetailDto;
-import com.profect.tickle.domain.performance.dto.response.PerformanceDto;
-import com.profect.tickle.domain.performance.dto.response.PerformanceHostDto;
+import com.profect.tickle.domain.performance.dto.response.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.query.Param;
 
@@ -48,4 +45,9 @@ public interface PerformanceMapper {
     Long findGenreIdByPerformanceId(@Param("performanceId") Long performanceId);
 
     List<PerformanceHostDto> findPerformancesByMemberId(Long memberId);
+
+    List<PerformanceScrapDto> findScrappedPerformancesByMemberId(Long memberId);
+
+    Boolean isScrapped(Long memberId, Long performanceId);
+
 }
