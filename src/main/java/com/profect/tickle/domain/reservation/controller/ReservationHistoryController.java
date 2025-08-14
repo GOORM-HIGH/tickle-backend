180bc05a8ee453e9642b6d92769d7cc4d8190de0
shimbaa <shim9597@gmail.com>
2025-08-14 17:33:15 +0900

    refactor: 예매 취소 HTTP 메서드 수정
    DELETE -> POST

--- src/main/java/com/profect/tickle/domain/reservation/controller/ReservationHistoryController.java
+++ src/main/java/com/profect/tickle/domain/reservation/controller/ReservationHistoryController.java
@@ -15,9 +15,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +61,7 @@ public class ReservationHistoryController {
     }
 
     @Operation(summary = "예매 취소", description = "예매를 취소합니다.")
-    @DeleteMapping("/{reservationId}")
+    @PostMapping("/{reservationId}")
     @PreAuthorize("hasRole('MEMBER')")
     public ResultResponse<ReservationCancelResponseDto> cancelReservation(
             @PathVariable Long reservationId) {
