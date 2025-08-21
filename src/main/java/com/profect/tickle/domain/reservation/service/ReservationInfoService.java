af1d20e56c314f4a6ec8a4de1ecb395b7a1a9fb9
shimbaa <shim9597@gmail.com>
2025-08-21 15:38:05 +0900

    refactor: 분리된 메서드로 선점 토큰 검증 및 총 결제 금액 계산 로직 정리
    - 기존 로직 내 중복 및 가독성 문제 해결
    - validatePreemptionToken, calculateTotalAmount 메서드 추가

--- src/main/java/com/profect/tickle/domain/reservation/service/ReservationInfoService.java
+++ src/main/java/com/profect/tickle/domain/reservation/service/ReservationInfoService.java
@@ -28,27 +28,24 @@ public class ReservationInfoService {
     private final CouponService couponService;
 
     public ReservationInfoResponseDto getReservationInfo(String preemptionToken) {
+        Long memberId = SecurityUtil.getSignInMemberId();
+
         // 1. 선점된 좌석들 조회
         List<Seat> seats = seatRepository.findByPreemptionToken(preemptionToken);
 
-        Long userId = SecurityUtil.getSignInMemberId();
+        // 2.선점 토큰 유효성 검증
+        validatePreemptionToken(seats, memberId);
 
-        if (seats.isEmpty() || !userId.equals(seats.getFirst().getMember().getId())) {
-            throw new BusinessException(ErrorCode.PREEMPTION_TOKEN_INVALID);
-        }
+        // 3. 총 결제 금액 계산
+        int totalAmount = calculateTotalAmount(seats);
 
-        // 2. 총 결제 금액 계산
-        int totalAmount = seats.stream()
-                .mapToInt(Seat::getSeatPrice)
-                .sum();
-
-        // 3. 사용자 보유 포인트 조회
+        // 4. 사용자 보유 포인트 조회
         int currentPoints = pointService.getCurrentPoint().credit();
 
-        // 4. 사용 가능한 쿠폰들 조회
-        List<CouponResponseDto> availableCoupons = couponService.getAvailableCoupons(userId);
+        // 5. 사용 가능한 쿠폰들 조회
+        List<CouponResponseDto> availableCoupons = couponService.getAvailableCoupons(memberId);
 
-        // 5. 좌석 정보 변환
+        // 6. 좌석 정보 변환
         List<PreemptedSeatInfo> seatInfos = seats.stream()
                 .map(this::convertToPreemptedSeatInfo)
                 .collect(Collectors.toList());
@@ -62,6 +59,23 @@ public class ReservationInfoService {
                 .preemptedUntil(seats.getFirst().getPreemptedUntil())
                 .build();
     }
+
+    private void validatePreemptionToken(List<Seat> seats, Long userId) {
+        if (seats.isEmpty()) {
+            throw new BusinessException(ErrorCode.PREEMPTION_TOKEN_INVALID);
+        }
+
+        if (!seats.getFirst().isOwnedBy(userId)) {
+            throw new BusinessException(ErrorCode.PREEMPTION_TOKEN_INVALID);
+        }
+    }
+
+    private int calculateTotalAmount(List<Seat> seats) {
+        return seats.stream()
+                .mapToInt(Seat::getSeatPrice)
+                .sum();
+    }
+
 
 
     private PreemptedSeatInfo convertToPreemptedSeatInfo(Seat seat) {
