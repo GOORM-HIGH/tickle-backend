bf3e274fb96acf83d9adc1cda1500c5ac83e9b57
Pangtaek <zombie3632@gmail.com>
2025-08-21 12:30:47 +0900

    refactor: EnableAsync 어노테이션 선언 위치 변경

--- src/main/java/com/profect/tickle/global/config/AsyncConfig.java
+++ src/main/java/com/profect/tickle/global/config/AsyncConfig.java
@@ -2,10 +2,12 @@ package com.profect.tickle.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
 
     @Bean(name = "mailExecutor")
