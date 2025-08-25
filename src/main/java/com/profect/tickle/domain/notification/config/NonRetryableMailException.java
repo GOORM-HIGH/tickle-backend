36af5bd02a28baded4ce834f6de05230909f7676
Pangtaek <zombie3632@gmail.com>
2025-08-25 15:20:29 +0900

    feat: 알림 전송 중단 에러 정의

--- src/main/java/com/profect/tickle/domain/notification/config/NonRetryableMailException.java
+++ src/main/java/com/profect/tickle/domain/notification/config/NonRetryableMailException.java
@@ -1,6 +1,12 @@
 package com.profect.tickle.domain.notification.config;
 
 public class NonRetryableMailException extends RuntimeException {
-    public NonRetryableMailException(String msg, Throwable cause) { super(msg, cause); }
-    public NonRetryableMailException(String msg) { super(msg); }
+
+    public NonRetryableMailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonRetryableMailException(String msg) {
+        super(msg);
+    }
 }
