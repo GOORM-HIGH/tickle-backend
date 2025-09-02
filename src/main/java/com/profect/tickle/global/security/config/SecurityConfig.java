508a5f8d1f9c98fec580dc03c0745b9bc718a4c0
Pangtaek <zombie3632@gmail.com>
2025-09-02 09:55:43 +0900

    fix: @Primary 추가로 CorsConfigurationSource 빈 충돌 해결

--- src/main/java/com/profect/tickle/global/security/config/SecurityConfig.java
+++ src/main/java/com/profect/tickle/global/security/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -48,6 +49,7 @@ public class SecurityConfig {
     private final Clock clock;
 
     @Bean
+    @Primary
     @Profile("prod")
     public CorsConfigurationSource prodCorsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
@@ -64,6 +66,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Primary
     @Profile("local")
     public CorsConfigurationSource localCorsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
