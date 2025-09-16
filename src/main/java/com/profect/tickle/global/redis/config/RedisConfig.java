4c6e33d257aad8a993184816d30860647a22fe09
omgomg333 <skdydtjd12@gmail.com>
2025-09-16 09:23:42 +0900

    feat: rabbitmq 성능 최적화

--- src/main/java/com/profect/tickle/global/redis/config/RedisConfig.java
+++ src/main/java/com/profect/tickle/global/redis/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -21,6 +22,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
