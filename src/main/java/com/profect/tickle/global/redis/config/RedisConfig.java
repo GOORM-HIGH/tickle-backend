6c60367c0f4bee0f982f46d8f8da5be9cfebc768
shimbaa <shim9597@gmail.com>
2025-09-17 17:55:36 +0900

    fix: Redis 설정 프로퍼티 경로를 Spring Boot 표준으로 수정
    - spring.redis.password → spring.data.redis.password로 변경
    - Spring Boot 자동 설정과 일관성 유지

--- src/main/java/com/profect/tickle/global/redis/config/RedisConfig.java
+++ src/main/java/com/profect/tickle/global/redis/config/RedisConfig.java
@@ -40,7 +40,7 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.redis.password}")
+    @Value("${spring.data.redis.password}")
     private String password;
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
