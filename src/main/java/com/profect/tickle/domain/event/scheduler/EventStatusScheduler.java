cdedc0c5cab7495128763905b72faaf6ce7d53d4
서해윤 <ggomulll3@gmail.com>
2025-10-16 22:00:23 +0900

    feat: 이벤트 스케쥴러의 Redis 필드 변경

--- src/main/java/com/profect/tickle/domain/event/scheduler/EventStatusScheduler.java
+++ src/main/java/com/profect/tickle/domain/event/scheduler/EventStatusScheduler.java
@@ -2,31 +2,37 @@ package com.profect.tickle.domain.event.scheduler;
 
 import com.profect.tickle.domain.event.entity.Event;
 import com.profect.tickle.domain.event.mapper.EventMapper;
+import com.profect.tickle.domain.event.repository.EventRepository;
 import com.profect.tickle.global.redis.service.RedisInitialize;
+import com.profect.tickle.global.status.StatusIds;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EventStatusScheduler {
 
-    private final EventMapper eventMapper;
+    private final EventRepository eventRepository;
     private final RedisInitialize redisInitialize;
 
-    @Scheduled(cron = "0 * * * * *") //매일 자정
+    @Scheduled(cron = "0 * * * * *")
     @Transactional
     public void syncEventStatuses() {
-        List<Event> eventsToStart = eventMapper.findEventsToMarkAsOngoing();
-        int toOngoing = eventMapper.markEventsAsOngoing();
-        int toFinished = eventMapper.markEventsAsFinished();
+        List<Event> eventsToStart = eventRepository.findEventsToStart(StatusIds.Event.SCHEDULED);
+
+        log.info("toOngoing={}개 이벤트 Redis 초기화 시작", eventsToStart.size());
 
         for (Event event : eventsToStart) {
-            redisInitialize.initializeRedisEvent(event); // Redis 초기화
+            if (event == null) continue;
+            log.info("Redis 초기화 대상 eventId={}, goalPrice={}", event.getId(), event.getGoalPrice());
+            redisInitialize.initializeRedisEvent(event);
         }
     }
 }
